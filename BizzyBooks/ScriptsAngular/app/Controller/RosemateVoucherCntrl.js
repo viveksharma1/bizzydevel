66b95a99fc41fef78b6ba488cf3b2ea6a3b0a9f4
vivek kumar <vivekkumarpatna@gmail.com>
2017-04-10 14:53:29 -0700

    add new file

---
+++ BizzyBooks/ScriptsAngular/app/Controller/RosemateVoucherCntrl.js
@@ -0,0 +1,35 @@
+﻿myApp.controller('RosemateVoucherCntrl', ['$scope', '$http', '$timeout', '$rootScope', '$state', function ($scope, $http, $timeout, $rootScope, $state) {
+
+    $(".my a").click(function (e) {
+        e.preventDefault();
+    });
+
+    $scope.goBack = function () {
+        window.history.back();
+    },
+
+    $('.filenameDiv').hide();
+    $('.attechmentDescription').hide();
+    $('.Attechmentdetail').click(function () {
+        $('.filenameDiv').show();
+        $("#name").append($("#NameInput").val());
+        $("#type").append($("#uploadBtn").val());
+
+    });
+
+    $('#removeattachment').click(function () {
+        $('.filenameDiv').hide();
+    });
+
+    $(":file").filestyle({ buttonName: "btn-sm btn-info" });
+
+
+    $('#Date').datepicker({
+        format: 'dd/mm/yyyy',
+        autoclose: true,
+    });
+
+
+
+
+}]);
